8ef9c4160a644fbc04e3320d099a2203a8e5bc89
Debora de Oliveira <deborapoh@gmail.com>
2023-05-24 00:18:19 -0300

    implements language menu part 1

---
+++ src/app/components/LanguageMenu.tsx
@@ -0,0 +1,41 @@
+import { Button, Menu, MenuItem } from "@mui/material";
+import { ReactNode, useState } from "react";
+
+const LanguageMenu = ({ children }: { children: ReactNode }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        {children}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Português</MenuItem>
+        <MenuItem onClick={handleClose}>English</MenuItem>
+      </Menu>
+    </div>
+  )
+}
+
+export default LanguageMenu
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { ThemeProvider, CssBaseline, Box, styled, Button } from '@mui/material'
+import { ThemeProvider, CssBaseline, Box, styled, Button, Link } from '@mui/material'
 import { RecoilRoot } from 'recoil'
 
 import './globals.css'
@@ -14,6 +14,7 @@ import languageGlobe from './images/language-globe.svg'
 import { useRouter, usePathname } from 'next/navigation'
 import classNames from 'classnames'
 import PageRouteButton from './components/PageRouteButton'
+import LanguageMenu from './components/LanguageMenu'
 
 const Container = styled(Box)(() => ({
   width: '100%',
@@ -61,30 +62,34 @@ export default function RootLayout({
             <Container>
               <Header>
                 <SocialMediaContainer>
-                  <Image
-                    className=""
-                    src={githubLogo}
-                    alt="Github image link"
-                    width={1000}
-                    height={1000}
-                    sizes="100vw"
-                    style={{
-                      height: 50,
-                      width: 50,
-                    }}
-                  />
-                  <Image
-                    className=""
-                    src={linkedinLogo}
-                    alt="Linkedin image link"
-                    width={1000}
-                    height={1000}
-                    sizes="100vw"
-                    style={{
-                      height: 50,
-                      width: 50,
-                    }}
-                  />
+                  <Link href="https://github.com/deborapoh" target="_blank" sx={{ textDecoration: 'none' }}>
+                    <Image
+                      className=""
+                      src={githubLogo}
+                      alt="Github image link"
+                      width={1000}
+                      height={1000}
+                      sizes="100vw"
+                      style={{
+                        height: 50,
+                        width: 50,
+                      }}
+                    />
+                  </Link>
+                  <Link href="https://www.linkedin.com/in/deborapoh" target="_blank" sx={{ textDecoration: 'none' }}>
+                    <Image
+                      className=""
+                      src={linkedinLogo}
+                      alt="Linkedin image link"
+                      width={1000}
+                      height={1000}
+                      sizes="100vw"
+                      style={{
+                        height: 50,
+                        width: 50,
+                      }}
+                    />
+                  </Link>
                 </SocialMediaContainer>
                 <PagesContainer>
                   <PageRouteButton pathname='/'>Home</PageRouteButton>
@@ -92,18 +97,20 @@ export default function RootLayout({
                   <PageRouteButton pathname='/resume'>Resume</PageRouteButton>
                   <PageRouteButton pathname='/projects'>Projects</PageRouteButton>
                   <PageRouteButton pathname='/contact'>Contact</PageRouteButton>
-                  <Image
-                    className=""
-                    src={languageGlobe}
-                    alt="Language globe image link"
-                    width={1000}
-                    height={1000}
-                    sizes="100vw"
-                    style={{
-                      height: 50,
-                      width: 50,
-                    }}
-                  />
+                  <LanguageMenu>
+                    <Image
+                      className=""
+                      src={languageGlobe}
+                      alt="Language globe image link"
+                      width={1000}
+                      height={1000}
+                      sizes="100vw"
+                      style={{
+                        height: 50,
+                        width: 50,
+                      }}
+                    />
+                  </LanguageMenu>
                 </PagesContainer>
               </Header>
               {children}
