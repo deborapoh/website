cd386c2ae686fd941dbcb80dc86050638d5c6a34
Debora de Oliveira <deborapoh@gmail.com>
2023-05-24 00:17:53 -0300

    implements resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,25 +1,78 @@
 'use client'
 
-import { Box, Typography, styled } from "@mui/material"
+import { Box, Divider, Link, Typography, styled } from "@mui/material"
+
+import jobs from '../jobs.json'
+
+const Container = styled(Box)(({ theme }) => ({
+  height: 'calc(100vh - 144px)',
+  paddingInline: 92,
+  paddingBlock: 88,
+  overflowY: 'scroll',
+}))
+
+const JobContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  marginBlock: 92,
+}))
 
 const Title = styled(Typography)(({ theme }) => ({
-  fontWeight: 'bold',
-  fontSize: 60,
+  fontSize: 64,
   color: theme.palette.common.black,
 }))
 
-const SubTitle = styled(Typography)(({ theme }) => ({
-  fontSize: 45,
-  color: theme.palette.secondary.main,
+const BlackBody = styled(Typography)(({ theme }) => ({
+  fontSize: 32,
+  color: theme.palette.common.black,
+  width: 1160,
+  textAlign: 'end',
+}))
+
+const PurpleBody = styled(Typography)(({ theme }) => ({
+  fontSize: 32,
+  color: theme.palette.primary.main,
+}))
+
+const Anchor = styled(Link)(({ theme }) => ({
+  textDecoration: 'none',
+  color: theme.palette.primary.main,
+  fontWeight: 'bold',
 }))
 
 export default function Resume() {
   return (
     <main>
-      <Box px={11.5} pt={6}>
-        <Title>Debora de Oliveira</Title>
-        <SubTitle>{'< developer >'}</SubTitle>
-      </Box>
+      <Container>
+        {jobs?.map((job) => {
+          const jobName = `${job.company.name} | ${job.company.location}`;
+
+          return (
+            <>
+              <JobContainer>
+                <Title>{job.title}</Title>
+                <BlackBody>
+                  {job.period} <br />
+                  <br />
+                  <PurpleBody><Anchor href={job.company.website}>{jobName}</Anchor></PurpleBody>
+                  <b>{job.location}</b> <br />
+                  {job.description.map((description) => (
+                    <>
+                      {description}
+                      <br />
+                    </>
+                  ))}
+                  <br />
+                  <PurpleBody>Softwares/Tools:</PurpleBody>
+                  {job.tools}
+                </BlackBody>
+              </JobContainer>
+              {job.last === 'false' && <Divider />}
+            </>
+          )
+        })}
+      </Container>
     </main>
   )
 }
