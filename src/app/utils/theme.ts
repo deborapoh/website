c56fa8e100eeefb644a09c4b07a71c39e9c58dbe
Debora de Oliveira <deborapoh@gmail.com>
2023-06-12 00:46:26 -0300

    fix window undefined 2

--- src/app/utils/theme.ts
+++ src/app/utils/theme.ts
@@ -2,14 +2,17 @@ import { Theme } from "@mui/material"
 import { desktopTheme, mobileTheme } from "../theme/theme"
 
 export const usePreferredTheme = (): Theme => {
-  window.addEventListener('resize', () => {
-    console.log('window.innerWidth', window.innerWidth)
-    if (typeof window !== "undefined" && window.innerWidth >= 1536) {
-      return desktopTheme
-    }
+  if (typeof window !== "undefined") {
+    window.addEventListener('resize', () => {
+      if (typeof window !== "undefined" && window.innerWidth >= 1536) {
+        return desktopTheme
+      }
 
-    return mobileTheme
-  })
+      return mobileTheme
+    })
 
-  return window.innerWidth >= 1536 ? desktopTheme : mobileTheme
+    return window.innerWidth >= 1536 ? desktopTheme : mobileTheme
+  }
+
+  return desktopTheme
 }
